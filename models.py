c612c1fcb81e3733520069a67dadfd158939e7f7
Julian Catan <109252676+RedM-LP@users.noreply.github.com>
2022-09-03 16:22:45 -0300

    Segunda Entrega
    Error - No encuentra la App AdidaZ. Y me genera un problema con el comando "makemigrations"

--- models.py
+++ models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 Class Remeras(models.Model):
     nombre = models.CharField(max_length=30)
-    fecha_lanzamiento = models.DateField()
+    fecha_de_ingreso = models.DateField()
+    stock_remera = models.IntegerField
 
-
-Class Pantalones(models.Model):
+Class EstiloRemera(models.Model):
     nombre = models.CharField(max_length=30)
     fecha_lanzamiento = models.DateField()
+    remera = models.ForeignKey('Remeras', on_delete=models.CASCADE)
